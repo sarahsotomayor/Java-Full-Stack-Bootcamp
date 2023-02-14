05b04aee2d0cdb6cbedd56de2506c7dc9edafdc5
Sarah <116047642+ellaeslaflaca@users.noreply.github.com>
2023-02-14 11:17:32 -0700

    Delete FirstJavaProgram directory

--- FirstJavaProgram/Demographics.java
+++
@@ -1,7 +0,0 @@
-public class Demographics{
-    public static void main(String[] args) {
-        System.out.println("My Name is Sarah");
-        System.out.println("I am 33 years young");
-        System.out.println("My hometown is Rio Rico, AZ");
-    }
-}
